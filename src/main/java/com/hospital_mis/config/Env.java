4b1c3de3ed0204c2a5134c729b446b6a569d635b
Steve <sntakirutimana72@gmail.com>
2025-05-11 23:19:45 +0200

    CLI API Added .env config reader

--- src/main/java/com/hospital_mis/config/Env.java
+++ src/main/java/com/hospital_mis/config/Env.java
@@ -2,15 +2,16 @@ package com.hospital_mis.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
-public class Env {
+public final class Env {
 
   private Env() {}
 
   private static final Dotenv dotenv = Dotenv.configure()
-    .filename(".env.local")
+    .filename("/src/main/resources/com/hospital_mis/.env.local")
     .load();
 
   public static final String DB_URL = dotenv.get("DB_URL"),
                              DB_USER = dotenv.get("DB_USER"),
                              DB_PASS = dotenv.get("DB_PASS");
+  public static final int QUERY_MAX_NUM_PER_PAGE = Integer.parseInt(dotenv.get("QUERY_MAX_NUM_PER_PAGE", "25"));
 }
