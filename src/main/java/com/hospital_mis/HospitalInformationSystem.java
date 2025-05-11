36294fb61a4a2674a6c4aff8d4011039afba6d3b
Steve <sntakirutimana72@gmail.com>
2025-05-11 12:14:16 +0200

    [Initial Commit] Update .gitignore Move fxml components into views dir Introduce application.properties

--- src/main/java/com/hospital_mis/HospitalInformationSystem.java
+++ src/main/java/com/hospital_mis/HospitalInformationSystem.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HospitalInformationSystem extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HospitalInformationSystem.class.getResource("index.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HospitalInformationSystem.class.getResource("views/index.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
