c046d00ed233a90ca7b85848927c2eb12b4d9b06
Steve <sntakirutimana72@gmail.com>
2025-05-11 11:52:26 +0200

    [Initial Commit] Set up dependencies Rename application main entry to `HospitalInformationSystem`

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.hospital_mis {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.hospital_mis to javafx.fxml;
+    exports com.hospital_mis;
+}
